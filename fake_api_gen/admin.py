09262a794794fdac6e13633b08ea4ed61a536fa6
Keyvan Mosharraf <kayvanmsh@gmail.com>
2014-06-21 14:12:05 -0700

    Users only see their own APIs or public ones

--- fake_api_gen/admin.py
+++ fake_api_gen/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.db.models import Q
 from fake_api_gen.models import FakeApi
+
 
 class FakeApiAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'date_modified', 'slug_link')
@@ -8,6 +10,17 @@ class FakeApiAdmin(admin.ModelAdmin):
         return u'<a target="__blank" href="/fake_api_gen/fakeapi/%s/">%s</a>' % (obj.slug, obj.slug)
     slug_link.allow_tags = True
     slug_link.short_description = "URL"
+
+    def queryset(self, request):
+        """Limit Pages to those that belong to the request's user."""
+        qs = super(FakeApiAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            # It is mine, all mine. Just return everything.
+            return qs
+        # Now we just add an extra filter on the queryset and
+        # we're done. Assumption: Page.owner is a foreignkey
+        # to a User.
+        return qs.filter(Q(owner=request.user) | Q(owner__isnull=True))
 
     # Uncomment to make the object automatically attach to the user who made it
     # def save_model(self, request, obj, form, change):

419ae428d272cc9843ec7ef32a9d2d7efb1ee60b
Keyvan Mosharraf <kayvanmsh@gmail.com>
2014-06-25 23:54:31 -0700

    changed the url for trackers api to have /api/ prefix

--- elkyrtools/urls.py
+++ elkyrtools/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^apidocs/', include('rest_framework_swagger.urls')),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^fake_api_gen/', include('fake_api_gen.urls')),
-    url(r'^tracker/', include('tracker.urls')),
+    url(r'^api/tracker/', include('tracker.urlsapi')),
 )

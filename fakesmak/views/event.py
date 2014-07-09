ee2183386622819f163b4345e417bb18d3fa3c02
Keyvan Mosharraf <kayvanmsh@gmail.com>
2014-07-08 23:59:21 -0700

    Add user to and from event

--- fakesmak/views/event.py
+++ fakesmak/views/event.py
@@ -1,4 +1,6 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from elkyrtools.viewsets import SLCGenericAPIViewMixin
 from fakesmak.models import Event
@@ -13,8 +15,28 @@ class EventViewSet(viewsets.ModelViewSet, SLCGenericAPIViewMixin):
     permission_classes = (permissions.IsAuthenticated, )
     owner = 'host'
 
+    @action()
+    def add_yourself_to_event(self, request, pk=None):
+        event = self.get_object()
+        event.attendees.add(request.user)
+        return Response({'status': 'user %s added to event %s' % (request.user, event)})
+
+    @action()
+    def remove_yourself_from_event(self, request, pk=None):
+        event = self.get_object()
+        if event.attendees.filter(username=request.user):
+            event.attendees.remove(request.user)
+            return Response({'status': 'user %s removed from event %s' % (request.user, event)})
+        else:
+            return Response({'error': 'user %s is not an attendee of event %s' % (request.user, event)},
+                            status=status.HTTP_412_PRECONDITION_FAILED)
+
     def post_save(self, *args, **kwargs):
+        """
+        Adds the event's host as one of the attendees and saves the tags sent with the request
+        """
+        saved_event = Event.objects.get(pk=self.object.pk)
+        saved_event.attendees.add(saved_event.host)
         if 'tags' in self.request.DATA:
-            saved_event = Event.objects.get(pk=self.object.pk)
             saved_event.tags.set(*self.request.DATA['tags'])
         return super(EventViewSet, self).post_save(*args, **kwargs)

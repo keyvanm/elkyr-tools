5ae4b942299c11fdc6433546ddb6625aa5bc396f
Keyvan Mosharraf <kayvanmsh@gmail.com>
2014-07-10 23:55:06 -0700

    Fixed a bug where having no profile pic caused a value error

--- fakesmak/fields.py
+++ fakesmak/fields.py
@@ -3,5 +3,7 @@ from rest_framework import serializers
 
 class AbsUrlFileField(serializers.Field):
     def to_native(self, file_object):
-        # return '%s%s' % (settings.MEDIA_URL, file_object)
-        return self.context['request'].build_absolute_uri(file_object.url)
+        try:
+            return self.context['request'].build_absolute_uri(file_object.url)
+        except ValueError:
+            return ""
